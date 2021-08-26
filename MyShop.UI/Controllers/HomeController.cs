5951b76c1edd3b4ec84f522b54b22b172173ed25
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-08-25 22:49:27 -0500

    Implemented Details page

--- MyShop.UI/Controllers/HomeController.cs
+++ MyShop.UI/Controllers/HomeController.cs
@@ -1,27 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 
 namespace MyShop.UI.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
-		{
-			return View();
+		IRepository<Product> context;
+		IRepository<ProductCategory> categoryContext;
+
+		public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext) {
+			context = productContext;
+			categoryContext = productCategoryContext;
 		}
 
-		public ActionResult About()
-		{
+		public ActionResult Index() {
+			var products = context.Collection().ToList();
+			return View(products);
+		}
+
+		public ActionResult Details(string id) {
+			var product = context.Find(id);
+			if (product == null)
+				return HttpNotFound();
+			return View(product);
+		}
+
+		public ActionResult About() {
 			ViewBag.Message = "Your application description page.";
 
 			return View();
 		}
 
-		public ActionResult Contact()
-		{
+		public ActionResult Contact() {
 			ViewBag.Message = "Your contact page.";
 
 			return View();
