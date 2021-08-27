b38254cefda583174fd2c1f548274625c920bf6c
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2021-08-26 22:08:21 -0500

    Implemented Customer registration

--- MyShop.DA.SQL/DataContext.cs
+++ MyShop.DA.SQL/DataContext.cs
@@ -14,5 +14,6 @@ namespace MyShop.DA.SQL
 		public DbSet<ProductCategory> ProductCategories { get; set; }
 		public DbSet<Basket> Baskets { get; set; }
 		public DbSet<BasketItem> BasketItems { get; set; }
+		public DbSet<Customer> Customers { get; set; }
 	}
 }
